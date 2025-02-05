8d89c89c522fade4f9f666ad2fa5cb824af85657
TheChuy1609 <blitzdashgd@gmail.com>
2025-02-05 11:32:01 -0700

    more stuff

--- render-build.sh
+++ render-build.sh
@@ -6,8 +6,16 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 npm install puppeteer
+set -e
+
+echo "Updating package lists..."
 apt-get update
+
+echo "Installing Chromium..."
 apt-get install -y chromium
+
+echo "Chromium installed at: $(which chromium)"
+
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
