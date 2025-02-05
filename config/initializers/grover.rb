80c0fb8efc8ecddc02e9abe20e44d05d25b6d05e
TheChuy1609 <blitzdashgd@gmail.com>
2025-02-05 11:39:29 -0700

    chromium install render

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,10 +1,8 @@
 Grover.configure do |config|
   config.options = {
-    executable_path: '/usr/bin/chromium', # Adjust if needed
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    format: 'A4',
-    margin: '0', 
+    format: "A4",
+    margin: "0",
     print_background: true,
-    display_url: 'http://localhost:3000'
+    display_url: "http://localhost:3000"
   }
 end
