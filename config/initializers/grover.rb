f5702436f762ca3c184161d920834be517d0a582
TheChuy1609 <blitzdashgd@gmail.com>
2025-02-05 21:24:57 -0700

    stuff

--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,6 +1,5 @@
 Grover.configure do |config|
   config.options = {
-    executable_path: Puppeteer.launch(options: { headless: true }).executable_path,
     format: "A4",
     margin: "0",
     print_background: true,
