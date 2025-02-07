c0f096442c8da056cbd6f202772ff0253c1a1e29
TheChuy1609 <blitzdashgd@gmail.com>
2025-02-06 19:32:55 -0700

    automatic button replacement from 'download' to 'main' a fter downloading

--- app/javascript/controllers/hide_buttons_controller.js
+++ app/javascript/controllers/hide_buttons_controller.js
@@ -1,14 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["button"]
-
-  connect() {
-    console.log("hide_buttons connected");
-  }
+  static targets = ["button"];
 
   replaceAfterClick() {
-    console.log("Download clicked, replacing button...");
-    this.buttonTarget.outerHTML = `<a href="/projects" class="px-6 py-2 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700">Main</a>`;
+    this.buttonTarget.outerHTML = `
+      <a href="/projects" class="px-6 py-2 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 text-center">
+        Main
+      </a>`;
   }
 }
